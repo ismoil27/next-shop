7ea5f12177ae20556158c8f3beecb10271fbf54a
ismoil27 <abduraimovismoil98@gmail.com>
2023-12-02 22:00:46 +0900

    feat: BRR initial commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="flex items-center px-4 md:px-12 py-2 justify-between fixed top-0 w-full z-50 shadow bg-white">
+      <Link href={"/"}>Logo</Link>
+      <div className="flex items-center space-x-2.5 text-sm">
+        <button className="button bg-blue-600 text-white border-transparent hover: border-blue-600 hover:bg-transparent hover:text-black">
+          Login
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            className="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;

fea936fa2ee8687e5bca2000fa08f7c2e4e47dc7
ismoil27 <abduraimovismoil98@gmail.com>
2023-12-03 14:56:42 +0900

    feat: Image optimization

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,19 +6,19 @@ const Navbar = () => {
     <header className="flex items-center px-4 md:px-12 py-2 justify-between fixed top-0 w-full z-50 shadow bg-white">
       <Link href={"/"}>Logo</Link>
       <div className="flex items-center space-x-2.5 text-sm">
-        <button className="button bg-blue-600 text-white border-transparent hover: border-blue-600 hover:bg-transparent hover:text-black">
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <Link href={"/"} className="mr-5 hover:text-gray-900">
+            Home Page{" "}
+          </Link>
+          <Link href={"/products"} className="mr-5 hover:text-gray-900">
+            All Products
+          </Link>
+        </nav>
+        <button className="button bg-blue-600 text-white border-transparent hover:border-blue-600 hover:bg-transparent hover:text-black">
           Login
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            className="w-4 h-4 ml-1"
-            viewBox="0 0 24 24"
-          >
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
+        </button>
+        <button className="button bg-transparent border-blue-600 hover:border-transparent hover:bg-blue-600 hover:text-white">
+          Sign up
         </button>
       </div>
     </header>
